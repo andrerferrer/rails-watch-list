54953a70393f7745e5c2a4d382fb33b1ff8da037
Marcy Hundschell Dela Cruz <marcy.delacruz@gmail.com>
2022-01-22 19:51:08 +0100

    watchlist

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,35 +1,34 @@
 class BookmarksController < ApplicationController
-  def index
-    @lists = List.all
-  end
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
   end
-end
 
-def create
-  @bookmark = Bookmark.new(bookmark_params)
-  @bookmark.list = @list
-  if @bookmark.save
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    flash[:notice] = @bookmark.errors.full_messages.to_sentence unless @bookmark.save
     redirect_to list_path(@list)
-  else
-    render template: 'lists/show'
   end
-end
 
-def destroy
-  @bookmark = Bookmark.find(params[:id])
-  @bookmark.destroy
-  redirect_to root_path
-end
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
 
-private
+  private
 
-def find_list
-  @list = List.find(params[:list_id])
-end
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 
-def bookmark_params
-  params.require(:bookmark).permit(:comment, :movie_id)
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
